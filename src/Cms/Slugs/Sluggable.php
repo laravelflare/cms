d0fb767f6cd15b190100bc0ba7d6828f5ec376e9
Aden Fraser <aden@jacobbailey.com>
2015-10-26 22:33:36 +0000

    Sluggable, SluggableRouter base, Slug Model amends, adds composer.json and Http folders

---
+++ src/Cms/Slugs/Sluggable.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace LaravelFlare\Cms\Slugs;
+
+trait Sluggable
+{
+
+}
---
+++ src/Cms/Slugs/SluggableRouter.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace LaravelFlare\Cms\Slugs;
+
+use Illuminate\Routing\Router;
+
+class SluggableRouter extends Router
+{
+
+}
