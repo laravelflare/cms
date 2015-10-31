9e90275345f52c3e9bdfd1759d15c72626d045e3
Aden Fraser <aden@jacobbailey.com>
2015-10-31 15:48:13 +0000

    SluggableModel Trait now provides the Morphsone relation

--- src/Cms/Slugs/SluggableModel.php
+++ src/Cms/Slugs/SluggableModel.php
@@ -4,4 +4,13 @@ namespace LaravelFlare\Cms\Slugs;
 
 trait SluggableModel
 {
+    /**
+     * Morph Slug to Model
+     * 
+     * @return 
+     */
+    public function slug()
+    {
+        return $this->morphOne(\LaravelFlare\Cms\Slugs\Slug::class, 'model');
+    }
 }
